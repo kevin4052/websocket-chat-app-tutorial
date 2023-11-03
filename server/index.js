bbdffe09eb4ab5f6246a4b4ab3c196d844c9226a
kevin4052 <kevin.hernandez001@gmail.com>
2023-11-02 20:01:37 -0500

    adding express to app, moving all files into server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,30 @@
-import { createServer } from 'http'
+import express from "express"
 import { Server } from 'socket.io'
+import path from "path"
+import { fileURLToPath } from "url"
 
-const httpServer = createServer();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname =  path.dirname(__filename)
+const PORT = process.env.PORT || 3500
 
-const socketServer = new Server(httpServer, {
+const app = express()
+
+// MIDDLEWARE
+app.use(express.static(path.join(__dirname, "public")))
+
+// START SERVER
+const expressServer = app.listen(PORT, () => {
+    console.log(`Listening on PORT: ${PORT}`)
+})
+
+// INITIALIZE SOCKET WITH CORS
+const socketServer = new Server(expressServer, {
     cors: {
         origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5501", "http://127.0.0.1:5501"]
     }
 })
 
+// TURNING ON SOCKET AND LISTEN FOR MESSAGE
 socketServer.on('connection', socket => {
     console.log(`User ${socket.id} connected`)
     
@@ -17,5 +33,3 @@ socketServer.on('connection', socket => {
         socketServer.emit('message', `${socket.id.substring(0, 5)}: ${data}`)
     })
 })
-
-httpServer.listen(3500, () => console.log('listening on port 3500'))
